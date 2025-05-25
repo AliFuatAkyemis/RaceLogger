54a16a52af3ad23ce64c739c0c7fa5b88fc0045a
alifuatakyemis <ali.fuat.akyemis@gmail.com>
2025-05-25 03:10:06 +0300

    Icon changed

--- src/frames/TemplateFrame.java
+++ src/frames/TemplateFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class TemplateFrame extends JFrame {
 	public TemplateFrame() {
-		ImageIcon icon = new ImageIcon("images/cartoon.png");
+		ImageIcon icon = new ImageIcon("images/akubitlogo.png");
 		this.setIconImage(icon.getImage());
 	}
 }
