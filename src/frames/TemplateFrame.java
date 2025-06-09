175fe003055e9f89646f7400b70d41694a6ce7e6
alifuatakyemis <ali.fuat.akyemis@gmail.com>
2025-06-09 03:36:38 +0300

    images folder content is changed

--- src/frames/TemplateFrame.java
+++ src/frames/TemplateFrame.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class TemplateFrame extends JFrame {
 	public TemplateFrame() {
-		ImageIcon icon = new ImageIcon("images/akubitlogo.png");
+		ImageIcon icon = new ImageIcon("images/icon.png");
 		this.setIconImage(icon.getImage());
 		this.setResizable(false);
 	}
