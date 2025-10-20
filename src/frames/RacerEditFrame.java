33a261e67e09a11c4f852cd4cc9c7cebd9f8ad2a
alifuatakyemis <ali.fuat.akyemis@gmail.com>
2025-10-20 19:05:20 +0300

    Comments are added

--- src/frames/RacerEditFrame.java
+++ src/frames/RacerEditFrame.java
@@ -285,7 +285,9 @@ public class RacerEditFrame extends TemplateFrame {
 		
 			while (row != null) {
 				String[] temp = row.split(","); //Simple split method to seperate ID and Name
-				model.addRow(new Object[] {Integer.valueOf(temp[0]), temp[1], temp[2]});
+                                String team = "none"; //team variable is initialized with a placeholder due to a missing information
+                                if (temp.length > 3) team = temp[3]; //updating the team team variable
+				model.addRow(new Object[] {Integer.valueOf(temp[0]), temp[1], temp[2], team});
 				row = reader.readLine(); //Update row with next line
 			}
 
